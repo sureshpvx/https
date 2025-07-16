c1965cb00711276db0fb102d5250fb696cabf800
Suresh Reelon <suresh.sharma@reelon.com>
2025-07-16 18:01:04 +0530

    Add Devise setup, dashboard controller, navbar, routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_devise_mapping, if: :devise_controller?
+
+  protected
+
+  def set_devise_mapping
+    @devise_mapping = Devise.mappings[:user]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+  end
 end
